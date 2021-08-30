8a798a229c216098cbeb0ff260210024b3c04a0d
Rohit Verma <rohit.verma03@nagarro.com>
2021-08-30 13:20:56 +0530

    setup done

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv').config()
+
+app.use(bodyParser.json())
+
+
+
+
+const mongoUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:?retryWrites=true&w=majority`
+
+console.log(mongoUri)
+
+
+mongoose.connect(mongoUri,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
+
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, ()=>{
+    console.log(`Server running on port ${port}`)
+})
+
+
