e6f1ffd7eeabd19c1ce30b31079e0524984f0bc7
Rohit Verma <verma.rohit.in@gmail.com>
2021-09-05 00:48:40 +0530

    signin

--- client/src/store/actions/user_actions.js
+++ client/src/store/actions/user_actions.js
@@ -19,8 +19,28 @@ export const registerUser = (values) =>{
             dispatch(index_actions.successGlobal('Welcome !! check your email and validate your account'));
         }
         catch(error){
-            console.log(error.response.data.message);
-            dispatch(index_actions.errorGlobal('Oops error registering'));
+            dispatch(index_actions.errorGlobal(error.response.data.message));
+        }
+    }
+}
+
+
+export const signInUser = (values) =>{
+    return async(dispatch)=>{
+        try{
+            const user = await axios.post(`/api/users/signin`,{
+                email: values.email,
+                password: values.password
+            });
+            
+            //dispatching an event to reducer to update the store with user data
+            dispatch(index_actions.authUser({data : user.data, auth: true}))
+
+
+            dispatch(index_actions.successGlobal('Welcome !!'));
+        }
+        catch(error){
+            dispatch(index_actions.errorGlobal(error.response.data.message));
         }
     }
 }
