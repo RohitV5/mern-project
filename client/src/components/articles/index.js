c8ed3118083b389358ffc712c2685537a6ae2843
Rohit Verma <verma.rohit.in@gmail.com>
2021-09-07 01:48:41 +0530

    scorecard component

--- client/src/components/articles/index.js
+++ client/src/components/articles/index.js
@@ -3,10 +3,12 @@ import React, {useEffect} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getArticle } from '../../store/actions/article_actions';
 import Loader from '../../utils/loader';
+import ScoreCard from '../../utils/scoreCard';
 
 const Article = (props) => {
 
     const { current } = useSelector(state => state.articles);
+    
     const dispatch = useDispatch();
 
     useEffect(()=>{
@@ -18,7 +20,7 @@ const Article = (props) => {
         { current ?
         
         <div className="article_container">
-            <div style ={{ backgraound:`url(https://picsum.photos/1920/1080)`}} className="image">
+            <div style={{ background:`url(https://picsum.photos/1920/1080)`}} className="image">
 
             </div>
             <h1>{current.title}</h1>
@@ -27,6 +29,8 @@ const Article = (props) => {
                     
                 </div> 
             </div>
+
+            <ScoreCard current={current}></ScoreCard>
 
         </div>        
     
